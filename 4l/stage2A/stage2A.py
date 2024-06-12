7912f6eb6562f1a0cfd9609034db1424bcb0d1d5
HindTaibi <137447124+hindtaibi@users.noreply.github.com>
2024-06-12 09:33:31 +0200

    Update stage2A.py

--- 4l/stage2A/stage2A.py
+++ 4l/stage2A/stage2A.py
@@ -1,40 +1,40 @@
-inputDir    = "../../../stage1bis/outputs"
+inputDir    = "../stage1ter/outputs"
 
-#In this case, we are looking for he final state Z1, Z2, Z3* -> ll (l1 + l2) ll (l3 + l4) jj (j5 + j6)
+
 
 #Optional: output directory, default is local dir
 outputDir   = "outputs"
 
 processList = {'wzp6_ee_mumuH_HZZ_ecm240':{},       #Signal
-               'wzp6_ee_mumuH_HWW_ecm240':{},
-               'wzp6_ee_mumuH_HZa_ecm240':{},
-               'wzp6_ee_mumuH_Haa_ecm240':{},
-               'wzp6_ee_mumuH_Hbb_ecm240':{},
-               'wzp6_ee_mumuH_Hcc_ecm240':{},
-               'wzp6_ee_mumuH_Hgg_ecm240':{},
-               'wzp6_ee_mumuH_Hmumu_ecm240':{},
-               'wzp6_ee_mumuH_Hss_ecm240':{},
-               'wzp6_ee_mumuH_Htautau_ecm240':{},
-               'wzp6_ee_nunuH_HZZ_ecm240':{},
-               'wzp6_ee_nunuH_HWW_ecm240':{},
-               'wzp6_ee_nunuH_HZa_ecm240':{},
-               'wzp6_ee_nunuH_Haa_ecm240':{},
-               'wzp6_ee_nunuH_Hbb_ecm240':{},
-               'wzp6_ee_nunuH_Hcc_ecm240':{},
-               'wzp6_ee_nunuH_Hgg_ecm240':{},   
-               'wzp6_ee_nunuH_Hmumu_ecm240':{},
-               'wzp6_ee_nunuH_Hss_ecm240':{},
-               'wzp6_ee_nunuH_Htautau_ecm240':{},
-               'wzp6_ee_eeH_HZZ_ecm240':{},         #Signal
-               'wzp6_ee_eeH_HWW_ecm240':{},
-               'wzp6_ee_eeH_HZa_ecm240':{},
-               'wzp6_ee_eeH_Haa_ecm240':{},
-               'wzp6_ee_eeH_Hbb_ecm240':{},
-               'wzp6_ee_eeH_Hcc_ecm240':{},
-               'wzp6_ee_eeH_Hgg_ecm240':{},
-               'wzp6_ee_eeH_Hmumu_ecm240':{},
-               'wzp6_ee_eeH_Hss_ecm240':{},
-               'wzp6_ee_eeH_Htautau_ecm240':{}
+               #'wzp6_ee_mumuH_HWW_ecm240':{},
+               #'wzp6_ee_mumuH_HZa_ecm240':{},
+               #'wzp6_ee_mumuH_Haa_ecm240':{},
+               #'wzp6_ee_mumuH_Hbb_ecm240':{},
+               #'wzp6_ee_mumuH_Hcc_ecm240':{},
+               #'wzp6_ee_mumuH_Hgg_ecm240':{},
+               #'wzp6_ee_mumuH_Hmumu_ecm240':{},
+               #'wzp6_ee_mumuH_Hss_ecm240':{},
+               #'wzp6_ee_mumuH_Htautau_ecm240':{},
+               #'wzp6_ee_nunuH_HZZ_ecm240':{},
+               #'wzp6_ee_nunuH_HWW_ecm240':{},
+               #'wzp6_ee_nunuH_HZa_ecm240':{},
+               #'wzp6_ee_nunuH_Haa_ecm240':{},
+               #'wzp6_ee_nunuH_Hbb_ecm240':{},
+               #'wzp6_ee_nunuH_Hcc_ecm240':{},
+               #'wzp6_ee_nunuH_Hgg_ecm240':{},   
+               #'wzp6_ee_nunuH_Hmumu_ecm240':{},
+               #'wzp6_ee_nunuH_Hss_ecm240':{},
+               #'wzp6_ee_nunuH_Htautau_ecm240':{},
+               #'wzp6_ee_eeH_HZZ_ecm240':{},         #Signal
+               #'wzp6_ee_eeH_HWW_ecm240':{},
+               #'wzp6_ee_eeH_HZa_ecm240':{},
+               #'wzp6_ee_eeH_Haa_ecm240':{},
+               #'wzp6_ee_eeH_Hbb_ecm240':{},
+               #'wzp6_ee_eeH_Hcc_ecm240':{},
+               #'wzp6_ee_eeH_Hgg_ecm240':{},
+               #'wzp6_ee_eeH_Hmumu_ecm240':{},
+               #'wzp6_ee_eeH_Hss_ecm240':{},
+               #'wzp6_ee_eeH_Htautau_ecm240':{}
                }
 
 
@@ -89,49 +89,16 @@ class RDFanalysis():
                .Filter("N_other_Z_leptons == 2 && N_on_Z_leptons ==2")                    
             
                #Z1 (the on shell Z) decays into 2 leptons
-               .Define("Za_inv_m",       "sqrt(on_Z_leptonic_tlv.E()*on_Z_leptonic_tlv.E()-on_Z_leptonic_tlv.P()*on_Z_leptonic_tlv.P())[0]")     #Invariant mass of the on shell Z
-               .Define("Za_e",           "on_Z_leptonic.E()[0]")
-               .Define("Za_energy",      "on_Z_leptonic.Energy()[0]")
-               .Define("Za_m",           "on_Z_leptonic.M()[0]")
-               .Define("Za_mag",         "on_Z_leptonic.Mag()[0]")
-               .Define("Za_P",           "on_Z_leptonic.P()[0]")
-               .Define("Za_recoil_m",    "on_Z_leptonic_recoil_m[0]")
-               
-               .Define("Z1_e",           "on_zed_leptonic_e[0]")
-               .Define("Z1_m",           "on_zed_leptonic_m[0]")
-               .Define("Z1_recoil_m",    "on_zed_leptonic_recoil_m[0]")
-               .Define("Z1_pt",          "on_zed_leptonic_pt[0]")
-               .Define("Z1_px",          "on_zed_leptonic_px[0]")
-               .Define("Z1_py",          "on_zed_leptonic_py[0]")
-               .Define("Z1_pz",          "on_zed_leptonic_pz[0]")
-               .Define("Z1_p",           "on_zed_leptonic_p[0]")
-               .Define("Z1_q",           "on_zed_leptonic_charge[0]")
-               .Define("Z1_phi",         "on_zed_leptonic_phi[0]")
-               .Define("Z1_theta",       "on_zed_leptonic_theta[0]")
+               .Define("Za_inv_m",          "sqrt(on_Z_leptonic_tlv.E()*on_Z_leptonic_tlv.E()-on_Z_leptonic_tlv.P()*on_Z_leptonic_tlv.P())[0]")     #Invariant mass of the on shell Z
+               .Define("Za_e",              "on_Z_leptonic.E()[0]")
+               .Define("Za_energy",         "on_Z_leptonic.Energy()[0]")
+               .Define("Za_m",              "on_Z_leptonic.M()[0]")
+               .Define("Za_mag",            "on_Z_leptonic.Mag()[0]")
+               .Define("Za_P",              "on_Z_leptonic.P()[0]")
+               .Define("Za_recoil_m",       "on_Z_leptonic_recoil_m[0]")
 
-               #Z2 decays into 2 leptons
-               .Define("Z2_e",           "on_zed_leptonic_e[1]")
-               .Define("Z2_m",           "on_zed_leptonic_m[1]")
-               .Define("Z2_recoil_m",    "on_zed_leptonic_recoil_m[1]")
-               .Define("Z2_pt",          "on_zed_leptonic_pt[1]")
-               .Define("Z2_px",          "on_zed_leptonic_px[1]")
-               .Define("Z2_py",          "on_zed_leptonic_py[1]")
-               .Define("Z2_pz",          "on_zed_leptonic_pz[1]")
-               .Define("Z2_p",           "on_zed_leptonic_p[1]")
-               .Define("Z2_q",           "on_zed_leptonic_charge[1]")
-               .Define("Z2_phi",         "on_zed_leptonic_phi[1]")
-               .Define("Z2_theta",       "on_zed_leptonic_theta[1]")
-
-               #Z3 decays into 2 jets
-               .Define("Z3",             "ReconstructedParticle::jetsum(jets_e2, jets_px2, jets_py2, jets_pz2)")
-               .Define("Z3_m",           "Z3.M()")
-               .Define("Z3_px",          "Z3.Px()")
-               .Define("Z3_py",          "Z3.Py()")
-               .Define("Z3_pz",          "Z3.Pz()")
-               .Define("Z3_p",           "Z3.P()")
-               .Define("Z3_pt",          "Z3.Pt()")
-               .Define("Z3_theta",       "Z3.Theta()")
-               .Define("Z3_phi",         "Z3.Phi()")
+               .Define("ll_tlv",            "on_Z_leptons_tlv[0] + on_Z_leptons_tlv[1]")
+               .Define("ll_m",              "ll_tlv.M()")
                
                #Durham N = 2
                .Define("j5_p",              "jets_p2[0]")
@@ -171,83 +138,6 @@ class RDFanalysis():
 
             #-------------------------------------------------------------------------------------------Photons
 
-            "N_photons",
-            "photons_e",
-            "photons_px",
-            "photons_py",
-            "photons_pz",
-            "photons_pt",
-            "photons_phi",
-            "photons_theta",
-            
-            #-------------------------------------------------------------------------------------------Leptons
-
-            #LooseLeptons_p0 are leptons with p>p0
-            "N_LooseLeptons_10",
-            "N_LooseLeptons_2",
-            "N_LooseLeptons_1",
-            "LooseLeptons_10_e",
-            "LooseLeptons_10_theta",
-            "LooseLeptons_10_phi",
-            "LooseLeptons_10_p",
-
-            #Taken leptons are the selected leptons without the extraleptons, i. e. the leptons that reconstructed the Z
-            "N_all_taken_leptons",
-            "all_taken_leptons_e",
-            "all_taken_leptons_p",
-            "all_taken_leptons_px",
-            "all_taken_leptons_py",
-            "all_taken_leptons_pz",
-            "all_taken_leptons_pt",
-            "all_taken_leptons_theta",
-            "all_taken_leptons_phi",
-            "all_taken_leptons_eta",
-            "all_taken_leptons_y",
-
-            #Angular difference of the leptons that reconstructed the Z bosons
-            "on_difftheta1_muons",
-            "on_difftheta2_muons",
-            "on_difftheta1_electrons",
-            "on_difftheta2_electrons",
-            "on_diffphi1_muons",
-            "on_diffphi2_muons",
-            "on_diffphi1_electrons",
-            "on_diffphi2_electrons",      
-            
-            #-------------------------------------------------------------------------------------------------Z
-
-            "Z1_e",
-            "Z1_m",
-            "Z1_recoil_m",
-            "Z1_pt",
-            "Z1_px",
-            "Z1_py",
-            "Z1_pz",
-            "Z1_p",
-            "Z1_q",
-            "Z1_phi",
-            "Z1_theta",
-
-            "Z2_e",
-            "Z2_m",
-            "Z2_recoil_m",
-            "Z2_pt",
-            "Z2_px",
-            "Z2_py",
-            "Z2_pz",
-            "Z2_p",
-            "Z2_q",
-            "Z2_phi",
-            "Z2_theta",
-            
-            "Z3_m",
-            "Z3_px",
-            "Z3_py",
-            "Z3_pz",
-            "Z3_p",
-            "Z3_pt",
-            "Z3_theta",  
-            "Z3_phi",
             
             #----------------------------------------------------------------------------------------------Jets
             
