2295a97bc65955dfb814fecd7c853ebb1b4aa560
HindTaibi <137447124+hindtaibi@users.noreply.github.com>
2024-06-26 11:07:39 +0200

    Update plotsBB.py

--- 4l/stage2B/plotsBB.py
+++ 4l/stage2B/plotsBB.py
@@ -82,13 +82,13 @@ extralabel["precuts_Za_m_Zb_m_emiss"] = "Precuts + 80 < m(Z_{a}) < 110 + 20 < m(
 colors = {}
 colors['Signal,HZZ'] = ROOT.kRed
 colors['HWW'] = ROOT.kGreen
-colors['nunuH,HZZ'] = ROOT.kBlue
+#colors['nunuH,HZZ'] = ROOT.kBlue
 colors['Hgg'] = ROOT.kYellow
 colors['nunuH,Hbb'] = ROOT.kGreen+3
 colors['Htautau'] = ROOT.kCyan+2
-colors['Hqq'] = ROOT.kPink-9
+colors['Hqq'] = ROOT.kPink-4
 colors['Hmumu'] = ROOT.kViolet
-colors['HZa'] = ROOT.kOrange+6
+colors['HZa'] = ROOT.kOrange+7
 colors['ZZ'] = ROOT.kAzure+6
 colors['WW'] = ROOT.kGray+1
 
@@ -98,7 +98,8 @@ plots['finalBB'] = {'signal':{'Signal,HZZ':['wzp6_ee_mumuH_HZZ_ecm240',
                                               'wzp6_ee_qqH_HZZ_ecm240',
                                               'wzp6_ee_ssH_HZZ_ecm240',
                                               'wzp6_ee_bbH_HZZ_ecm240',
-                                              'wzp6_ee_ccH_HZZ_ecm240']},
+                                              'wzp6_ee_ccH_HZZ_ecm240',
+					      'wzp6_ee_nunuH_HZZ_ecm240']},
 
                    'backgrounds':{'HWW':['wzp6_ee_mumuH_HWW_ecm240', 
                                          'wzp6_ee_eeH_HWW_ecm240'],
@@ -112,7 +113,7 @@ plots['finalBB'] = {'signal':{'Signal,HZZ':['wzp6_ee_mumuH_HZZ_ecm240',
                                   'HZa':['wzp6_ee_mumuH_HZa_ecm240', 
                                          'wzp6_ee_eeH_HZa_ecm240'],
                                          
-                                  'nunuH,HZZ':['wzp6_ee_nunuH_HZZ_ecm240'],
+                                  'nunuH,Hbb':['wzp6_ee_nunuH_Hbb_ecm240'],
                                   
                                   'Hqq':['wzp6_ee_mumuH_Hbb_ecm240', 
                                          'wzp6_ee_eeH_Hbb_ecm240',
@@ -132,7 +133,7 @@ plots['finalBB'] = {'signal':{'Signal,HZZ':['wzp6_ee_mumuH_HZZ_ecm240',
 legend = {}
 legend['Signal,HZZ'] = 'Signal'
 legend['HWW'] = 'H#rightarrowWW'
-legend['nunuH,HZZ'] = '#nu#nuH, HZZ'
+#legend['nunuH,HZZ'] = '#nu#nuH, HZZ'
 legend['nunuH,Hbb'] = '#nu#nuH, Hbb'
 legend['Htautau'] = 'H#rightarrow#tau#tau'
 legend['Hmumu'] = 'H#rightarrow#mu#mu'
