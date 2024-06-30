29de6813ee7810d5598c7c3d0873c60d67f50f04
HindTaibi <137447124+hindtaibi@users.noreply.github.com>
2024-06-30 18:28:11 +0200

    Update finalAB.py

--- 4l/stage2A/finalAB.py
+++ 4l/stage2A/finalAB.py
@@ -7,7 +7,7 @@ inputDir  = "../outputs/"
 outputDir  = "outputs"
 
 processList = {#Signal
-	       'wzp6_ee_mumuH_HZZ_ecm240':{},   
+	           'wzp6_ee_mumuH_HZZ_ecm240':{},   
                'wzp6_ee_eeH_HZZ_ecm240':{},     
                'wzp6_ee_nunuH_HZZ_ecm240':{},   
                'wzp6_ee_qqH_HZZ_ecm240':{},
@@ -62,9 +62,9 @@ doScale = False
 
 #Dictionnay of the list of cuts. The key is the name of the selection that will be added to the output file
 cutList = {"precuts":				   "emiss > -10",
-	   "precuts_Za_m":			   "Za_m < 110 && Za_m > 80",
-	   "precuts_Za_m_Zb_m":			   "Za_m < 110 && Za_m > 80 && Zb_m < 110 && Zb_m > 80",
-           "precuts_Za_m_Zb_m_emiss":              "Za_m < 110 && Za_m > 80 && Zb_m < 110 && Zb_m > 80 && emiss > 8"
+	       "precuts_Za_m":			   "Za_m < 110 && Za_m > 80",
+	       "precuts_Za_m_Zb_m":	       "Za_m < 110 && Za_m > 80 && Zb_m < 110 && Zb_m > 80",
+           "precuts_Za_m_Zb_m_emiss":  "Za_m < 110 && Za_m > 80 && Zb_m < 110 && Zb_m > 80 && emiss > 8"
            }
 
 #Dictionary for the ouput variable/hitograms. The key is the name of the variable in the output files. "name" is the name of the variable in the input file, "title" is the x-axis label of the histogram, "bin" the number of bins of the histogram, "xmin" the minimum x-axis value and "xmax" the maximum x-axis value.
@@ -80,6 +80,8 @@ histoList = {
     
     "ll1a_m":         {"name":"ll1a_m",            "title":"Mass of the 1st Dilepton + Photon [GeV]",          "bin":100,  "xmin":60,     "xmax":150},
     "ll2a_m":         {"name":"ll2a_m",            "title":"Mass of the 2nd Dilepton + Photon [GeV]",          "bin":100,  "xmin":60,     "xmax":150},
+    "ll1a_recoil_m":  {"name":"ll1a_recoil_m",     "title":"Recoil Mass of the 1st Dilepton + Photon [GeV]",   "bin":100,  "xmin":90,     "xmax":170},
+    "ll2a_recoil_m":  {"name":"ll2a_recoil_m",     "title":"Recoil Mass of the 2nd Dilepton + Photon [GeV]",   "bin":100,  "xmin":100,    "xmax":170},
 
     "ll1jj_m":        {"name":"ll1jj_m",           "title":"Mass of the 1st Dilepton + Dijet [GeV]",           "bin":100,  "xmin":60,     "xmax":180},
     "ll2jj_m":        {"name":"ll2jj_m",           "title":"Mass of the 2nd Dilepton + Dijet [GeV]",           "bin":100,  "xmin":60,     "xmax":180},
@@ -97,36 +99,36 @@ histoList = {
     
     #Za (1st dilepton) and Zb (2nd dilepton)
     
-    "Za_e":           {"name":"Za_e",              "title":"Za_e [GeV]",               	         "bin":100,  "xmin":80,     "xmax":120},
-    "Za_p":           {"name":"Za_p",              "title":"Za_p [GeV]",               			 "bin":100,  "xmin":0,      "xmax":80},
-    "Za_px":          {"name":"Za_px",             "title":"Za_px [GeV]",               		 "bin":100,  "xmin":-70,    "xmax":70},
-    "Za_py":          {"name":"Za_py",             "title":"Za_py [GeV]",               		 "bin":100,  "xmin":-70,    "xmax":70},
-    "Za_pz":          {"name":"Za_pz",             "title":"Za_pz [GeV]",               		 "bin":100,  "xmin":-70,    "xmax":70},
-    "Za_theta":       {"name":"Za_theta",          "title":"Za_theta",               			 "bin":100,  "xmin":0,      "xmax":3.5},
-    "Za_phi":         {"name":"Za_phi",            "title":"Za_phi",               			     "bin":100,  "xmin":-3.5,   "xmax":3.5},
-    "Za_m":           {"name":"Za_m",              "title":"Za_m [GeV]",               			 "bin":70,   "xmin":70,     "xmax":120},
-    "Za_recoil_m":    {"name":"Za_recoil_m",       "title":"Za_recoil_m [GeV]",                  "bin":100,  "xmin":90,     "xmax":170},
+    "Za_e":           {"name":"Za_e",              "title":"E(Z_{a}) [GeV]",           	         "bin":100,  "xmin":80,     "xmax":120},
+    "Za_p":           {"name":"Za_p",              "title":"p(Z_{a}) [GeV]",        			 "bin":100,  "xmin":0,      "xmax":80},
+    "Za_px":          {"name":"Za_px",             "title":"p_{x}(Z_{a}) [GeV]",           		 "bin":100,  "xmin":-70,    "xmax":70},
+    "Za_py":          {"name":"Za_py",             "title":"p_{y}(Z_{a}) [GeV]",           		 "bin":100,  "xmin":-70,    "xmax":70},
+    "Za_pz":          {"name":"Za_pz",             "title":"p_{z}(Z_{a}) [GeV]",           		 "bin":100,  "xmin":-70,    "xmax":70},
+    "Za_theta":       {"name":"Za_theta",          "title":"#theta(Z_{a})",            			 "bin":100,  "xmin":0,      "xmax":3.5},
+    "Za_phi":         {"name":"Za_phi",            "title":"#phi(Z_{a})",        			     "bin":100,  "xmin":-3.5,   "xmax":3.5},
+    "Za_m":           {"name":"Za_m",              "title":"m(Z_{a}) [GeV]",           			 "bin":70,   "xmin":70,     "xmax":120},
+    "Za_recoil_m":    {"name":"Za_recoil_m",       "title":"m_{recoil}(Z_{a}) [GeV]",            "bin":100,  "xmin":90,     "xmax":170},
     
-    "Zb_e":           {"name":"Zb_e",              "title":"Zb_e [GeV]",               			 "bin":100,  "xmin":80,     "xmax":120},
-    "Zb_p":           {"name":"Zb_p",              "title":"Zb_p [GeV]",               			 "bin":100,  "xmin":0,      "xmax":80},
-    "Zb_px":          {"name":"Zb_px",             "title":"Zb_px [GeV]",               		 "bin":100,  "xmin":-60,    "xmax":60},
-    "Zb_py":          {"name":"Zb_py",             "title":"Zb_py [GeV]",               		 "bin":100,  "xmin":-60,    "xmax":60},
-    "Zb_pz":          {"name":"Zb_pz",             "title":"Zb_pz [GeV]",               		 "bin":100,  "xmin":-60,    "xmax":60},
-    "Zb_theta":       {"name":"Zb_theta",          "title":"Zb_theta",               			 "bin":100,  "xmin":0,      "xmax":3.5},
-    "Zb_phi":         {"name":"Zb_phi",            "title":"Zb_phi",               			     "bin":100,  "xmin":-3.5,   "xmax":3.5},
-    "Zb_m":           {"name":"Zb_m",              "title":"Zb_m [GeV]",               			 "bin":100,  "xmin":70,     "xmax":120},
-    "Zb_recoil_m":    {"name":"Zb_recoil_m",       "title":"Zb_recoil_m [GeV]",                  "bin":100,  "xmin":100,    "xmax":170},
+    "Zb_e":           {"name":"Zb_e",              "title":"E(Z_{b}) [GeV]",           			 "bin":100,  "xmin":80,     "xmax":120},
+    "Zb_p":           {"name":"Zb_p",              "title":"p(Z_{b}) [GeV]",           			 "bin":100,  "xmin":0,      "xmax":80},
+    "Zb_px":          {"name":"Zb_px",             "title":"p_{x}(Z_{b}) [GeV]",           		 "bin":100,  "xmin":-60,    "xmax":60},
+    "Zb_py":          {"name":"Zb_py",             "title":"p_{y}(Z_{b}) [GeV]",           		 "bin":100,  "xmin":-60,    "xmax":60},
+    "Zb_pz":          {"name":"Zb_pz",             "title":"p_{z}(Z_{b}) [GeV]",           		 "bin":100,  "xmin":-60,    "xmax":60},
+    "Zb_theta":       {"name":"Zb_theta",          "title":"#theta(Z_{b})",           			 "bin":100,  "xmin":0,      "xmax":3.5},
+    "Zb_phi":         {"name":"Zb_phi",            "title":"#phi(Z_{b})",          			     "bin":100,  "xmin":-3.5,   "xmax":3.5},
+    "Zb_m":           {"name":"Zb_m",              "title":"m(Z_{b}) [GeV]",           			 "bin":100,  "xmin":70,     "xmax":120},
+    "Zb_recoil_m":    {"name":"Zb_recoil_m",       "title":"m_{recoil}(Z_{b}) [GeV]",            "bin":100,  "xmin":100,    "xmax":170},
     
     #Dijet
   
-    "jj_e":           {"name":"jj_e",          "title":"jj_e [GeV]",               			 "bin":100,  "xmin":0,     "xmax":60},
-    "jj_p":           {"name":"jj_p",          "title":"jj_p [GeV]",               			 "bin":100,  "xmin":0,     "xmax":50},
-    "jj_px":          {"name":"jj_px",         "title":"jj_px [GeV]",               		 "bin":100,  "xmin":-40,   "xmax":40},
-    "jj_py":          {"name":"jj_py",         "title":"jj_py [GeV]",               		 "bin":100,  "xmin":-40,   "xmax":40},
-    "jj_pz":          {"name":"jj_pz",         "title":"jj_pz [GeV]",               		 "bin":100,  "xmin":-40,   "xmax":40},
-    "jj_theta":       {"name":"jj_theta",      "title":"jj_theta",               			 "bin":100,  "xmin":0,     "xmax":3.5},
-    "jj_phi":         {"name":"jj_phi",        "title":"jj_phi",               			     "bin":100,  "xmin":-3.5,  "xmax":3.5},
-    "jj_m":           {"name":"jj_m",          "title":"jj_m [GeV]",               			 "bin":100,  "xmin":0,     "xmax":50},
+    "jj_e":           {"name":"jj_e",          "title":"E(jj) [GeV]",             			 "bin":100,  "xmin":0,     "xmax":60},
+    "jj_p":           {"name":"jj_p",          "title":"p(jj) [GeV]",              			 "bin":100,  "xmin":0,     "xmax":50},
+    "jj_px":          {"name":"jj_px",         "title":"p_{x}(jj) [GeV]",             		 "bin":100,  "xmin":-40,   "xmax":40},
+    "jj_py":          {"name":"jj_py",         "title":"p_{y}(jj) [GeV]",              		 "bin":100,  "xmin":-40,   "xmax":40},
+    "jj_pz":          {"name":"jj_pz",         "title":"p_{z}(jj) [GeV]",              		 "bin":100,  "xmin":-40,   "xmax":40},
+    "jj_theta":       {"name":"jj_theta",      "title":"#theta(jj)",               			 "bin":100,  "xmin":0,     "xmax":3.5},
+    "jj_phi":         {"name":"jj_phi",        "title":"#phi(jj)",             			     "bin":100,  "xmin":-3.5,  "xmax":3.5},
+    "jj_m":           {"name":"jj_m",          "title":"m(jj) [GeV]",             			 "bin":100,  "xmin":0,     "xmax":50},
     
     "diffthetajets_56": {"name":"diffthetajets_56",   "title":"diffthetajets_56",               "bin":100,  "xmin":0,     "xmax":3.5},
     "diffphijets_56":   {"name":"diffphijets_56",     "title":"diffphijets_56",                 "bin":100,  "xmin":0,     "xmax":7},
@@ -139,6 +141,7 @@ histoList = {
     "pzmiss":         {"name":"pzmiss",         "title":"Missing p_{z} [GeV]",               			 "bin":100,  "xmin":-40,   "xmax":40},
 
     "visible_m":      {"name":"visible_m",      "title":"Visible Mass [GeV]",                            "bin":100,  "xmin":170,   "xmax":250},
+    "missing_m":      {"name":"missing_m",      "title":"Missing Mass [GeV]",                            "bin":100,  "xmin":0,     "xmax":50},
 }
 
 
